5f367a061bae12e81d7346731241eb76dadefdad
Colin Walters <walters@verbum.org>
2021-08-02 17:05:24 -0400

    C9S: Disable secure boot tests
    TODO: Make this declarative since we hit it with RHCOS too.

--- ci/build-test-qemu.sh
+++ ci/build-test-qemu.sh
@@ -6,5 +6,18 @@ dn=$(dirname $0)
 export COSA_SKIP_OVERLAY=1
 # Create a temporary cosa workdir
 cd "$(mktemp -d)"
-cosa init /src
-exec ${dn}/prow-build-test-qemu.sh
+cosa init "${tmpsrc}"/src
+# Grab the raw value of `mutate-os-release` and use sed to convert the value
+# to X-Y format
+# ocpver=$(rpm-ostree compose tree --print-only src/config/manifest.yaml | jq -r '.["mutate-os-release"]' | sed 's|\.|-|')
+# curl -L http://base-"${ocpver}"-rhel8.ocp.svc.cluster.local > src/config/ocp.repo
+cosa fetch
+cosa build
+cosa buildextend-extensions
+# FIXME C9S no secure boot
+# cosa kola --basic-qemu-scenarios
+cosa kola run basic
+cosa kola run 'ext.*'
+# TODO: all tests in the future, but there are a lot
+# and we want multiple tiers, and we need to split them
+# into multiple pods and stuff.
