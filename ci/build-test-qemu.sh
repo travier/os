b96bbf474ed82c9400dd63cea42e8c7f6d2a106e
Timothée Ravier <tim@siosm.fr>
2022-07-05 19:24:16 +0200

    ci: Remove unused scripts & Dockerfile
    - Remove previous CI scripts have been replaced by the unified script.
    - Remove CoreOS layering tests that are not enabled for this branch.

--- ci/build-test-qemu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Temporarily keep this script to let us merge over green CI.
-# Can be removed once we've merged the openshift/release changes.
-set -xeuo pipefail
-dn="$(dirname $0)"
-# Prow jobs don't support adding emptydir today
-export COSA_SKIP_OVERLAY=1
-exec "${dn}/prow-entrypoint.sh" "rhcos-86-build-test-qemu"
--- ci/validate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-dn="$(dirname $0)"
-exec "${dn}/prow-entrypoint.sh" "validate"
